971ec6c3eb1666aa6d2a2d0913c1440c6468bf2e
Boris <morcow6@gmail.com>
2022-04-29 04:01:03 +0800

    move CalendarDay array generation and other logic in calendar service

---
+++ src/app/service/calendar.service.ts
@@ -0,0 +1,87 @@
+import { Injectable } from '@angular/core';
+import * as moment from "moment";
+import {Humidity, Measurement, MeasurementDay, Pressure, Temperature} from "../model/MeasurementDay";
+import {DateService} from "./date.service";
+import {TestDataJson} from "../data/TestDataJson";
+
+export interface CalendarDay {
+  value: moment.Moment
+  today: boolean
+  disabled: boolean
+  selected: boolean
+  avgMeasurements: Measurement[]
+  monthStart: boolean
+}
+
+export interface CalendarWeek {
+  days: CalendarDay[]
+}
+
+@Injectable({
+  providedIn: 'root'
+})
+export class CalendarService {
+
+  //TODO This is inject (database) job
+  measurementDays = TestDataJson.getDataFromJson()
+  // measurementDays = TestData.generateRandomData(6, 6)
+  calendar: CalendarWeek[] = [];
+
+  constructor(private dateService: DateService) {
+    this.dateService.date.subscribe(this.generateCalendar.bind(this))
+  }
+
+  generateCalendar(now: moment.Moment): void  {
+    const startOfMonth = now.clone().startOf('month')
+    const startDay = startOfMonth.startOf('week')
+    const endDay = now.clone().endOf('month').endOf('week')
+
+    const date = startDay.clone().subtract(1, 'day')
+
+    const calendar = []
+
+    while (date.isBefore(endDay) && calendar.length < 6) {
+      calendar.push({
+        days: Array(7)
+          .fill(0)
+          .map(() => {
+            const value = date.add(1, 'day').clone()
+            const today = moment().isSame(value, 'date') && moment().isSame(value, 'month')
+            const selected = !now.isSame(value, 'date')
+            const avgMeasurements = this.getAvgMeasurementsForDate(value)
+            const disabled = avgMeasurements.length == 0
+            const monthStart = value.date() == 1
+
+            return {
+              value, today, disabled, selected, avgMeasurements, monthStart
+            }
+          })
+      })
+    }
+
+    this.calendar = calendar
+  }
+
+  getAvgMeasurementsForDate(value: moment.Moment): Measurement[] {
+    const foundMeasureDay = this.findMeasurementDay(value)
+    return (foundMeasureDay === undefined) ? [] as Measurement[] : foundMeasureDay.avgMeasurements
+  }
+
+  findMeasurementDay(value: moment.Moment): MeasurementDay|undefined {
+    return this.measurementDays.find((date: MeasurementDay) =>
+      //if the number of measure day and month is not equal to processing calendar day then return undefined
+      date.number === value.date() && date.month === value.format('MMMM') && date.year === value.year())
+  }
+
+  checkInstanceTemperature(object: any) {
+    return object instanceof Temperature
+  }
+
+  checkInstancePressure(object: any) {
+    return object instanceof Pressure
+  }
+
+  checkInstanceHumidity(object: any) {
+    return object instanceof Humidity
+  }
+}
--- src/app/views/calendar/calendar.component.ts
+++ src/app/views/calendar/calendar.component.ts
@@ -1,25 +1,10 @@
-import {Component, OnInit} from '@angular/core';
+import {Component, OnDestroy, OnInit} from '@angular/core';
 import {DateService} from "../../service/date.service";
-import * as moment from "moment";
 import 'moment/locale/ru'
-import {MeasurementDay, Humidity, Pressure, Temperature, Measurement} from "../../model/MeasurementDay";
 import {MatDialog} from '@angular/material/dialog'
 import {DialogComponent} from "../dialog/dialog.component";
-import {TestDataJson} from "../../data/TestDataJson";
-import {TestData} from "../../data/TestData";
-
-interface CalendarDay {
-  value: moment.Moment
-  today: boolean
-  disabled: boolean
-  selected: boolean
-  avgMeasurements: Measurement[]
-  monthStart: boolean
-}
-
-interface Week {
-  days: CalendarDay[]
-}
+import {CalendarDay, CalendarService} from "../../service/calendar.service";
+import {MeasurementDay, Temperature} from "../../model/MeasurementDay";
 
 @Component({
   selector: 'app-calendar',
@@ -28,78 +13,25 @@ interface Week {
 })
 export class CalendarComponent implements OnInit {
 
-  calendar: Week[] = [];
-  //TODO This is inject (database) job
+  checked = false;
 
-  // measurementDays = TestData.generateRandomData(6, 6)
-  measurementDays = TestDataJson.getDataFromJson()
-
-  constructor(private dateService: DateService, private dialog: MatDialog) { }
+  constructor(public calendarService: CalendarService,
+              private dateService: DateService,
+              private dialog: MatDialog) { }
 
   ngOnInit(): void {
-    this.dateService.date.subscribe(this.generateCalendar.bind(this))
-    console.log(JSON.stringify(this.measurementDays))
+  }
+
+  changeToggle() {
+    this.checked = !this.checked
   }
 
   openDialog(disabled: boolean, day: CalendarDay) {
     if (!disabled) {
       let dialogRef = this.dialog.open(DialogComponent)
       let instance = dialogRef.componentInstance
-      instance.clickedDay = this.findMeasurementDay(day.value, this.measurementDays)!
+      instance.clickedDay = this.calendarService.findMeasurementDay(day.value) ?? MeasurementDay.prototype
     }
   }
 
-  generateCalendar(now: moment.Moment) {
-    const startOfMonth = now.clone().startOf('month')
-    const startDay = startOfMonth.startOf('week')
-    const endDay = now.clone().endOf('month').endOf('week')
-
-    const date = startDay.clone().subtract(1, 'day')
-
-    const calendar = []
-
-    while (date.isBefore(endDay) && calendar.length < 6) {
-      calendar.push({
-        days: Array(7)
-          .fill(0)
-          .map(() => {
-            const value = date.add(1, 'day').clone()
-            const today = moment().isSame(value, 'date') && moment().isSame(value, 'month')
-            const selected = !now.isSame(value, 'date')
-            const avgMeasurements = this.getAvgMeasurementsForDate(value)
-            const disabled = avgMeasurements.length == 0
-            const monthStart = value.date() == 1
-
-            return {
-              value, today, disabled, selected, avgMeasurements, monthStart
-            }
-          })
-      })
-    }
-
-    this.calendar = calendar
-  }
-
-  getAvgMeasurementsForDate(value: moment.Moment): Measurement[] {
-    const foundMeasureDay = this.findMeasurementDay(value, this.measurementDays)
-    return (foundMeasureDay === undefined) ? [] as Measurement[] : foundMeasureDay.avgMeasurements
-  }
-
-  findMeasurementDay(value: moment.Moment, measurementDays: MeasurementDay[]): MeasurementDay|undefined {
-    return measurementDays.find((date: MeasurementDay) =>
-      //if the number of measure day and month is not equal to processing calendar day then return undefined
-      date.number === value.date() && date.month === value.format('MMMM') && date.year === value.year())
-  }
-
-  checkInstanceTemperature(object: any) {
-    return object instanceof Temperature
-  }
-
-  checkInstancePressure(object: any) {
-    return object instanceof Pressure
-  }
-
-  checkInstanceHumidity(object: any) {
-    return object instanceof Humidity
-  }
 }
