92dae5e878d390eb8c9f012e55987affb789917e
Boris <morcow6@gmail.com>
2022-04-30 04:01:21 +0800

    add sort-days-ascending pipe

---
+++ src/app/pipes/sort-days-ascending.pipe.ts
@@ -0,0 +1,26 @@
+import { Pipe, PipeTransform } from '@angular/core';
+import {MeasurementDay} from "../model/MeasurementDay";
+import * as moment from "moment";
+
+@Pipe({
+  name: 'sortDaysAsc'
+})
+export class SortDaysAscendingPipe implements PipeTransform {
+
+  months = ['январь', 'февраль', 'март', 'апрель', 'май', 'июнь',
+    'июль', 'август', 'сентябрь', 'октябрь', 'ноябрь', 'декабрь'];
+
+  transform(days: MeasurementDay[]): MeasurementDay[] {
+    const sortingArray: MeasurementDay[] = [];
+    days.forEach(val => sortingArray.push(Object.assign({}, val)))
+
+    return sortingArray.sort(
+      (a, b): number => {
+        const aDate = [a.year, this.months.indexOf(a.month), a.number]
+        const bDate = [b.year, this.months.indexOf(b.month), b.number]
+
+        return moment(aDate).diff(moment(bDate))
+      }
+    )
+  }
+}
