ff443e4ca4ce9e1a11b80650293c22310b31c2ec
Boris <morcow6@gmail.com>
2022-05-03 06:01:24 +0800

    add year-selector component

---
+++ src/app/views/year-selector/year-selector.component.ts
@@ -0,0 +1,30 @@
+import { Component, OnInit } from '@angular/core';
+import {AvailableDaysService} from "../../service/available-days.service";
+
+@Component({
+  selector: 'app-year-selector',
+  templateUrl: './year-selector.component.html',
+  styleUrls: ['./year-selector.component.scss']
+})
+export class YearSelectorComponent implements OnInit {
+
+  //TODO improve css design:
+  // -css file of component duplicate another css file it's not good
+  // -common style enchants
+  // -make disable pointer when achieved edge of array
+  // -fix grid CSS item height (now items of others months have different height, especially - May)
+
+  year: string
+
+  constructor(public availableDaysService: AvailableDaysService) {
+    availableDaysService.currentYear.subscribe(value => this.year = value.name)
+  }
+
+  ngOnInit(): void {
+  }
+
+  changeYear(direction: number) {
+    this.availableDaysService.changeCurrentYear(direction)
+  }
+
+}
