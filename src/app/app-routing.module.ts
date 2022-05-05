7170f70ae54d7e5b8d79c725f859e03195bc9381
Boris <morcow6@gmail.com>
2022-05-06 06:01:14 +0800

    add routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import {YaMapComponent} from "./views/ya-map/ya-map.component";
+import {CalendarComponent} from "./views/calendar/calendar.component";
+import {MeasurementsTableComponent} from "./views/measurements-table/measurements-table.component";
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', component: MeasurementsTableComponent},
+  {path: 'calendar', component: CalendarComponent},
+  {path: 'map', component: YaMapComponent}
+]
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
